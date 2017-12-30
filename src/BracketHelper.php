a0353cd7c89fe7ae1c4bf369351ec146631824f8
Deniss Kozickis <deniss.kozickis@gmail.com>
2017-12-30 09:43:14 +0200

    * Remove English explanations * Brackets as const for readability * LICENSE file * Version 1.0.1

--- src/BracketHelper.php
+++ src/BracketHelper.php
@@ -13,12 +13,13 @@ namespace Dkozickis;
 
 class BracketHelper
 {
+    // Используем константы, немного легче читаемо в if/elseif в isBalanced()
+    const OPENING_BRACKET = '(';
+    const CLOSING_BRACKET = ')';
+
     /**
-     * Main entry function.
      * Основная публичная функция.
      *
-     * Check if string contains valid symbols first. Throws InvalidArgumentException if not.
-     * If string contains all valid symbols, checks if string is balanced.
      * Сначала проверяет содержит ли строка только валидные символы, если нет - кидает InvalidArgumentException.
      * Если все символы валидны - проверят сбалансирована ли строка.
      *
@@ -31,14 +32,13 @@ class BracketHelper
     public function isValid(string $string): bool
     {
         if ($this->containsInvalidSymbols($string)) {
-            throw new \InvalidArgumentException('String contains invalid symbols');
+            throw new \InvalidArgumentException('Строка содержит недопустимые символы.');
         }
 
         return $this->isBalanced($string);
     }
 
     /**
-     * Checks if string is balanced each "(" has ")".
      * В балансе ли строка (йода стайл везде лезет). Для каждой скобки "(" должна быть закрывающая ")".
      *
      * @param string $string
@@ -50,18 +50,14 @@ class BracketHelper
         $balance = 0;
 
         for ($i = 0; $i < strlen($string); ++$i) {
-            // We can access string as array.
-            // We don't care about multibyte string because we filtered string with invalid symbols already.
             // К строке можно применять доступ как к массиву.
             // Нас не заботит, что в строке может быть мултьтибайт символ т.к. мы бы уже кинули InvalidArgumentException.
             $character = $string[$i];
 
-            if ('(' === $character) {
-                // If opening bracket - increase balance.
+            if (self::OPENING_BRACKET === $character) {
                 // Открывашка - увеличить баланс.
                 ++$balance;
-            } elseif (')' === $character) {
-                // If closing balance - decrease balance.
+            } elseif (self::CLOSING_BRACKET === $character) {
                 // Закрывашка - уменшить баланс.
                 --$balance;
             }
@@ -73,10 +69,6 @@ class BracketHelper
             }
         }
 
-        // Return whether the brackets in the string are balanced.
-        // If we had more closing brackets then early "return false;" was already done
-        // This will return false if we had more opening brackets.
-        // This will return true if brackets are balanced
         // Строка в балансе или нет.
         // Если было больше закрывашек - мы уже вернули false раньше.
         // Тут false будет если больше открывашек.
@@ -85,10 +77,8 @@ class BracketHelper
     }
 
     /**
-     * Check if string contains only "(", ")", "\t", "\r", "\n", whitespace "\s".
      * Проверяет, что сторока содержит только разрешенные символы - "(", ")", "\t", "\r", "\n", пробел "\s".
      *
-     * Function returns true if string contains invalid symbols. False otherwise.
      * Функций возвращает true если в строке есть невалидные символы, false если нет.
      * Просто люблю regexp.
      *
@@ -98,10 +88,8 @@ class BracketHelper
      */
     private function containsInvalidSymbols(string $string): bool
     {
-        // preg_match will return false if there are invalid symbols
-        // We inverse that with "!"
         // Тут preg_match вернет false если есть невалидные симолы
-        // Надо инвертировать это при помощи негации "!"
+        // Надо инвертировать это при помощи "!"
         return !preg_match("/^([()\r\n\t\s]+)$/", $string);
     }
 }
